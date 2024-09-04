79d1a15f078f3370efbf137c655c4f54f87d1fc2
davai <davai@gmail.com>
2024-09-03 21:10:38 -0300

    Att

--- todos/views.py
+++ todos/views.py
@@ -2,7 +2,14 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from .models import Todo
 
 
-def home(request):
-    return render(request, "home.html")
+def todo_list_old(request):
+    nome = 'rodrigo'
+    alunos = ['1.Ana','2.Jose', '3.Carlos']
+    return render(request, "todos/todo_list.html", {"nome":nome, "lista_alunos":alunos})
+
+def todo_list(request):
+    todos = Todo.objects.all()
+    return render(request, "todos/todo_list.html", {"todos": todos})
